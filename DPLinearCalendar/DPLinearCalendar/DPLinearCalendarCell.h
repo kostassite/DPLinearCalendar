7076ccf0cb0c9ed206dfe08777ce78bf75570bc8
Kostas Antonopoulos <kostas@devpro.gr>
2012-09-28 11:16:59 +0300

    Added DPLinearCalendarCell

---
+++ DPLinearCalendar/DPLinearCalendar/DPLinearCalendarCell.h
@@ -0,0 +1,14 @@
+//
+//  DPLinearCalendarCell.h
+//  DPLinearCalendar
+//
+//  Created by Kostas Antonopoulos on 9/28/12.
+//  Copyright (c) 2012 Kostas Antonopoulos. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DPLinearCalendarCell : UIView
+@property (weak, nonatomic) IBOutlet UILabel *dateLabel;
+
+@end
---
+++ DPLinearCalendar/DPLinearCalendar/DPLinearCalendarCell.m
@@ -0,0 +1,31 @@
+//
+//  DPLinearCalendarCell.m
+//  DPLinearCalendar
+//
+//  Created by Kostas Antonopoulos on 9/28/12.
+//  Copyright (c) 2012 Kostas Antonopoulos. All rights reserved.
+//
+
+#import "DPLinearCalendarCell.h"
+
+@implementation DPLinearCalendarCell
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
