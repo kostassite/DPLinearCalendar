0d9be74f9e97a864202af975d90b755fbfc384b5
Kostas Antonopoulos <kostas@devpro.gr>
2012-09-28 11:06:47 +0300

    Initial Commit

---
+++ DPLinearCalendar/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  DPLinearCalendar
+//
+//  Created by Kostas Antonopoulos on 9/28/12.
+//  Copyright (c) 2012 Kostas Antonopoulos. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
